c7588c221b45ba43843c42dce4b4a9204d472a46
theandersonn <theandersonn@gmail.com>
2022-07-06 15:41:12 -0300

    estrutura do componente navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <nav id="navbar">
-        <h2><Link to='/'>MoviesLib</Link></h2>
-        <Link to='/movie/1'>Movie</Link>
-        <Link to='/search'>Search</Link>
-      </nav>
+      <Navbar />
       <Outlet />
     </div>
   )
