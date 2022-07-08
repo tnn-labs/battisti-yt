02c727c90f7f08b5b39c7c4ff9060be866290ef4
theandersonn <theandersonn@gmail.com>
2022-07-08 12:23:54 -0300

    marcação da estrutura do componente MovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import MovieCard from '../components/MovieCard'
 
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
@@ -17,9 +18,15 @@ const Home = () => {
     getTopRatedMovies(topRatedUrl)
   }, [])
 
-  console.log(topMovies)
-
-  return <div>Home</div>
+  return (
+    <div className='container'>
+      <h2 className='title'>Melhores filmes:</h2>
+      <div className='movies-container'>
+        {topMovies.length === 0 && <p>Carregando...</p>}
+        {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  )
 }
 
 export default Home
