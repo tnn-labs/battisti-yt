1f4743133991ed2ca5913642a92d2da23212d9bc
theandersonn <theandersonn@gmail.com>
2022-07-08 23:02:58 -0300

    implementa metodo buscar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,20 @@
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import { BiCameraMovie, BiSearchAlt2 } from 'react-icons/bi'
 
 import './Navbar.css'
 
 const Navbar = () => {
+  const [search, setSearch] = useState('')
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!search) return
+    navigate(`/search?q=${search}`)
+    setSearch('')
+  }
+
   return (
     <nav id="navbar">
       <h2>
@@ -12,8 +23,8 @@ const Navbar = () => {
             MoviesLib
           </Link>
       </h2>
-      <form>
-        <input type='text' placeholder='Busque um filme' />
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Busque um filme' onChange={(e) => setSearch(e.target.value)} value={search} />
         <button type='submit'>
           <BiSearchAlt2 />
         </button>
