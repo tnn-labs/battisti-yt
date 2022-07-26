552f82e692a7bab945af48dd3aca0fe026b3e35a
theandersonn <theandersonn@gmail.com>
2022-07-26 09:44:08 -0300

    portifolio: inicio da sidebar

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,6 +1,16 @@
+import Avatar from '../img/john-doe.jpeg'
+
+import '../styles/components/sidebar.sass'
+
 const Sidebar = () => {
   return(
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <img src={Avatar} alt="John Doe" />
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="" className="btn">Download currículo</a>
+    </aside>
   );
 };
 
