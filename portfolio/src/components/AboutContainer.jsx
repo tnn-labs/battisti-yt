89bd4892fbf99c8e2aa79863d4bbec4ff9b3aeb6
theandersonn <theandersonn@gmail.com>
2022-07-28 14:13:42 -0300

    portfolio: implementa conteudo principal

---
+++ portfolio/src/components/AboutContainer.jsx
@@ -0,0 +1,11 @@
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui doloribus autem, consequuntur maxime fugiat voluptas magnam quae, quod quo corporis eaque veniam? Quisquam commodi architecto quibusdam distinctio veniam sed explicabo.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui doloribus autem, consequuntur maxime fugiat voluptas magnam quae, quod quo corporis eaque veniam? Quisquam commodi architecto quibusdam distinctio veniam sed explicabo.</p>
+    </section>
+  )
+}
+
+export default AboutContainer
---
+++ portfolio/src/components/ProjectsContainer.jsx
@@ -0,0 +1,11 @@
+const ProjectsContainer = () => {
+  return (
+    <section>
+      <h2>Projetos</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis, quo? Ut, esse? Odit officiis quam quibusdam quisquam in, optio facilis distinctio a est temporibus possimus, excepturi rerum dolor, aspernatur totam.</p>
+      <a href="#" className="btn">Ver Projetos</a>
+    </section>
+  )
+}
+
+export default ProjectsContainer
